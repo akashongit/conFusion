1da27bf8b0e1552b0db720f2cd83c0bc04ad0e6f
akashongit <akashgeevarghese.mec@gmail.com>
2018-06-19 15:38:41 +0530

    reactive forms part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { Feedback,ContactType } from '../shared/feedback';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -11,6 +12,35 @@ export class ContactComponent implements OnInit {
   feedbackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
+
+  formErrors = {
+    'firstname':'',
+    'lastname':'',
+    'telnum':'',
+    'email':''
+  };
+
+  validationMessages = {
+    'firstname': {
+      'required':      'First Name is required.',
+      'minlength':     'First Name must be at least 2 characters long.',
+      'maxlength':     'FirstName cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required':      'Last Name is required.',
+      'minlength':     'Last Name must be at least 2 characters long.',
+      'maxlength':     'Last Name cannot be more than 25 characters long.'
+    },
+    'telnum': {
+      'required':      'Tel. number is required.',
+      'pattern':       'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required':      'Email is required.',
+      'email':         'Email not in valid format.'
+    },
+  };
+
   constructor(private fb: FormBuilder) {
     this.createForm();
   }
@@ -20,15 +50,41 @@ export class ContactComponent implements OnInit {
 
   createForm(){
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname:['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', 
+      [Validators.required, Validators.minLength(2), Validators.maxLength(25) ]],
+      lastname:['', 
+      [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0, 
+      [Validators.required, Validators.pattern ]],
+      email: ['', 
+      [Validators.required, Validators.email ]],
       agree: false,
       contactType: 'None',
       message: ''
     });
 
+    this.feedbackForm.valueChanges
+    .subscribe( data => this.onValueChanged(data));
+
+    this.onValueChanged();
+    // reset form validation messages
+
+  }
+
+  onValueChanged(data?: any){
+    if(!this.feedbackForm)
+    { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors){
+      this.formErrors[field]='';
+      const control = form.get(field);
+      if(control && control.dirty && !control.valid){
+        const messages = this.validationMessages[field];
+        for (const key in control.errors){
+          this.formErrors[field] += messages[key]+ ' ';
+        }
+      }
+    }
   }
 
   onSubmit(){
