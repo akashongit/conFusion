82f0e7d89901fe86ff574d6b29ea6779afbbce4b
akashongit <akashgeevarghese.mec@gmail.com>
2018-06-22 10:33:22 +0530

    build without testing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,11 @@ import {MatDialog, MatDialogRef} from '@angular/material';
 })
 export class LoginComponent implements OnInit {
 
-  user = {remember:false};
+  user = {
+    username: '',
+    password: '',
+    remember:false
+  };
   constructor(private dialogRef: MatDialogRef<LoginComponent>) { }
 
   ngOnInit() {
